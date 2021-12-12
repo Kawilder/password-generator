127e43446a2fa0249e2b635d24e53af38c780137
Kawilder <kevin_wilde564@yahoo.com>
2021-12-12 16:54:53 -0600

    added random charecter functions.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+//Random generators
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()-_=+[]{},.<>;:\|';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+//functiion start
+var generatePassword = function() {
+  userInput = window.prompt("Please select at least one (can select more than one) of these parameters to genertate a password, (use spaces between): 1=lowercase 2=uppercase 3=numbers 4=symbols.");
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
